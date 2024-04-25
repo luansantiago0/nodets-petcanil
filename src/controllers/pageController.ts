41c15c47a73dff58d5f3443d2e6d393750c80a36
luansantiago0 <luansantiago0200@gmail.com>
2024-04-25 14:44:36 -0300

    chore: base created

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+  // res.render('pages/page');
+};
+
+export const dogs = (req: Request, res: Response) => {
+  // res.render('pages/dogs');
+};
+
+export const cats = (req: Request, res: Response) => {
+  // res.render('pages/cats');
+};
+
+export const fishes = (req: Request, res: Response) => {
+  // res.render('pages/fishes');
+};
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import mustache from "mustache-express";
+import path from "path";
+import mainRoutes from "./routes/index";
+import { Request, Response } from "express";
+
+dotenv.config();
+
+const server = express();
+
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
+
+server.use(express.static(path.join(__dirname, "../public")));
+
+// Rotas
+server.use(mainRoutes);
+server.use((req: Request, res: Response) => {
+  res.send("Página não encontrada");
+});
+
+server.listen(process.env.PORT);
